b6758c4e56add3ca936d01281c91583f33de3243
Hazal Savran <hazalsavran@gmail.com>
2025-05-22 00:06:47 +0300

    edits

--- Frontends/MovieApi.WebUI/Controllers/MovieController.cs
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -30,7 +30,15 @@ namespace MovieApi.WebUI.Controllers
 
         public async Task<IActionResult> MovieDetail(int id)
         {
-            id = 0;
+            ViewBag.pageType = "MovieDetail";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7016/api/Movie/GetMovie?id="+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultMovieDto>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
